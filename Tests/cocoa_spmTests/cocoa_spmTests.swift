ddeda34c1c6c3bc53a7e109b793132457cef0f83
Armen Hakobyan <armen.hakobyan@softconstruct.com>
2022-10-31 15:09:11 +0400

    Release 0.0.1.

---
+++ Tests/cocoa_spmTests/cocoa_spmTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import cocoa_spm
+
+final class cocoa_spmTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(cocoa_spm().text, "Hello, World!")
+    }
+}
